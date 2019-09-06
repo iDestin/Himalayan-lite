aab9094195790373c50d180d282ccd9bcda6832e
china德鲁 <1725194818@qq.com>
2019-09-06 17:15:07 +0800

    更新登录后的界面

--- pages/user/user.js
+++ pages/user/user.js
@@ -5,56 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    //获得设备信息
+    wx.getSystemInfo({
+      success (res) {
+        console.log(res.windowHeight);
+        that.setData({
+          phoneHeight:res.windowHeight,
+        })
+      }
+    })
+    // 查看是否授权
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo);
+              that.setData({
+                login:true,
+                avatarUrl:res.userInfo.avatarUrl,
+                nickName:res.userInfo.nickName
+              })
+            }
+          })
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  // 获取用户的头像和昵称信息
+  bindGetUserInfo(e) {
+    var that = this;
+    console.log(e.detail.userInfo)
+    that.setData({
+      login:true,
+      avatarUrl:e.detail.userInfo.avatarUrl,
+      nickName:e.detail.userInfo.nickName
+    })
+    
   },
 
   /**
