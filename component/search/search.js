395234631600191e51a7d871bb17d5d0b621b092
chinadelu <1725194818@qq.com>
2019-12-15 23:35:04 +0800

    '增加组件'

---
+++ component/search/search.js
@@ -0,0 +1,31 @@
+//Component Object
+Component({
+    properties: {
+        myProperty:{
+            type:String,
+            value:'',
+            observer: function(){}
+        },
+    },
+    data: {
+
+    },
+    methods: {
+        
+    },
+    created: function(){
+
+    },
+    attached: function(){
+
+    },
+    ready: function(){
+
+    },
+    moved: function(){
+
+    },
+    detached: function(){
+
+    },
+});
