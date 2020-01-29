9c692c25441f1ebc096192acd76dc7f0c4b0a8a4
chinadelu <1725194818@qq.com>
2020-01-29 21:03:34 +0800

    update readme.md

--- app.js
+++ app.js
@@ -2,17 +2,6 @@
 let height = 0;
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
     // 获取用户信息
     wx.getSetting({
       success: res => {
